1f99f2ee4e1417cc5da66fbd5dd9d22b80773af7
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-23 17:51:26 -0300

    add router

---
+++ src/Router/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login/index";
+import Profile from "../pages/Profile";
+import Repo from "../pages/Repo";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repo />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
