596516e69d2cf5b755c5812b1102d5511073e043
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-23 15:52:03 -0300

    criando o componente profile

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { Container, Main, LeftSide, RightSide } from "./styles";
+
+import ProfileData from "../../components/ProfileData";
+
+const Profile: React.FC = () => {
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            // Username
+            userName={"username"}
+            // Full Name
+            name={"name"}
+            // Avatar
+            avatarUrl={"avatar"}
+            // Followers
+            followers={"number"}
+            // Following
+            following={"number"}
+            // Company
+            company={"company"}
+            // Location
+            location={"location"}
+            // email
+            email={"email"}
+            // Blog
+            blog={"null"}
+          />
+        </LeftSide>
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
+};
+
+export default Profile;
---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.main``;
+
+export const Main = styled.div``;
+
+export const LeftSide = styled.div``;
+
+export const RightSide = styled.div``;
