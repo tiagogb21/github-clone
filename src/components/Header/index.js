60457ed480aec3c3556e8fa50c4ff7decca913bf
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-23 16:05:26 -0300

    "bugfix"

--- src/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./styles";
-
-function Header() {
-  return <div />;
-}
-
-export default Header;
--- src/pages/Main/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./styles.css";
-
-function Main() {
-  return <div />;
-}
-
-export default Main;
